011bf911b4359473b58253f37cfb7ca2f75cc2d0
m_fadly_as <fadlyahmadsehabudin@gmail.com>
2025-05-15 07:26:59 +0700

    Add files via upload

---
+++ formsucsespage2.js
@@ -0,0 +1,11 @@
+
+//  untuk animasi atau redirection setelah beberapa detik.
+$(document).ready(function() {
+    // Contoh redirect otomatis setelah 5 detik
+    setTimeout(function() {
+        window.location.href = "Beranda.html";
+    }, 5000);
+});
+
+
+
